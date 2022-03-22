255fbb9a1e60bbad1618478b55201f53d5cad258
Poorvang1353 <poorvangpatel13@gmail.com>
2022-03-22 10:00:48 +0530

    poorvang

---
+++ Simple programs/pelindrome.py
@@ -0,0 +1,13 @@
+# from re import I
+
+
+i = int(input("Enter the number: "))
+rev = 0
+x = i
+while(i>0):
+    rev = (rev*10)+i%10
+    i = i//10
+if(x==rev):
+    print(x," is pelindrome number")
+else:
+    print(x," is not pelindrome number")
