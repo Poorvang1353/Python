dbee7f8b93656590363180ee5da98b15b2f5ec53
Poorvang1353 <poorvangpatel13@gmail.com>
2022-04-18 13:00:22 +0530

    poorvang

--- Simple programs/syllabus of college/2-practical.py
+++ Simple programs/syllabus of college/2-practical.py
@@ -1,18 +1,3 @@
-def add(a, b):
-    return a + b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    return a / b
-
 
 print("Select operation.")
 print("1.add")
@@ -25,17 +10,17 @@ while True:
     choise = input("Enter choise(1/2/3/4): ")
 
     if choise in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number"))
-        num2 = float(input("Enter second number"))
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
 
         if choise == '1':
-             print(num1, '+', num2, '=', add(num1, num2))
+             print(num1, '+', num2, '=', (num1+num2))
         elif choise == '2':
-            print(num1, '-', num2, '=', subtract(num1, num2))
+            print(num1, '-', num2, '=', (num1-num2))
         elif choise == '3':
-            print(num1, '*', num2, '=', multiply(num1, num2))
+            print(num1, '*', num2, '=', (num1*num2))
         elif choise == '4':
-            print(num1, '/', num2, '=', divide(num1, num2))
+            print(num1, '/', num2, '=', (num1/num2))
 
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
