564a5ac818931649a0efcdb1162cac84c9d81a55
Poorvang1353 <poorvangpatel13@gmail.com>
2022-04-22 09:16:48 +0530

    poorvang

---
+++ Simple programs/syllabus of college/16_practical.py
@@ -0,0 +1,13 @@
+class Circle():
+    def __init__(self, r):
+        self.radius = r
+
+    def area(self):
+        return self.radius**2*3.14
+    
+    def perimeter(self):
+        return 2*self.radius*3.14
+
+NewCircle = Circle(8)
+print(NewCircle.area())
+print(NewCircle.perimeter())
