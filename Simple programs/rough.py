64684e07b01c70158d196b1197dd4eaba6276858
Poorvang1353 <poorvangpatel13@gmail.com>
2022-04-23 18:12:28 +0530

    poorvang

--- Simple programs/rough.py
+++ Simple programs/rough.py
@@ -1,30 +1,14 @@
+class circle():
+    def __init__(self,r):
+        self.radius = r
+
+    def area(self):
+        return self.radius**2*3.14
+
+    def perimeter(self):
+        return 2*3.14*self.radius
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+newCircle = circle(8)
+print(newCircle.area)
+print(newCircle.perimeter) 
--- Simple programs/syllabus of college/16_practical.py
+++ Simple programs/syllabus of college/16_practical.py
@@ -4,9 +4,10 @@ class Circle():
 
     def area(self):
         return self.radius**2*3.14
-    
+
     def perimeter(self):
         return 2*self.radius*3.14
+
 
 NewCircle = Circle(8)
 print(NewCircle.area())
