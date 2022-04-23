3f00151fd75d457f150531b479a35965e0d46730
Poorvang1353 <poorvangpatel13@gmail.com>
2022-04-23 18:35:26 +0530

    poorvang

--- Simple programs/syllabus of college/14_practical.py
+++ Simple programs/syllabus of college/14_practical.py
@@ -8,17 +8,16 @@ def duplicate(dupli_list):
             main_list.append(i)
     return main_list
 
-lst = [1,2,3,2,5,3,2,7,3,6]
+
+lst = [1, 2, 3, 2, 5, 3, 2, 7, 3, 6]
 print("Origenal list: ", lst)
 dupli_list = lst
 
-print("Duplicates Removed list: ",duplicate(dupli_list))
-
-
+print("Duplicates Removed list: ", duplicate(dupli_list))
 
 
 # #self practice
-# #this one uses loop 
+# #this one uses loop
 # def using_loop(x):
 #   num = []
 #   for i in x:
